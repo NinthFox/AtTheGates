5b6ae2b99d38c0f0ef3480e3ad8ccd6dbe0f2c4d
samy <samy.v@thewalkingnerds.com>
2019-08-13 23:26:47 +0200

    Last changes before dodo

---
+++ Assets/InputManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+
+    public LayerMask ground;
+
+    public void MovePlayer(InputAction.CallbackContext context){
+        RaycastHit hit;
+        
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
+            Debug.Log("Hit");
+            Player.Instance.agent.destination = hit.point;
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+    #region Singleton
+    public static Player Instance {get {return _instance;} }
+    private static Player _instance;
+    #endregion
+
+    public CinemachineFreeLook playerCamera;
+    public Transform playerCharacter;
+    public NavMeshAgent agent;
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Debug.LogWarning("Player instance already exists, deleting new one");
+            Destroy(this.gameObject);
+        } else {
+            _instance = this;
+        }
+        playerCamera = transform.GetChild(0).GetComponent<CinemachineFreeLook>();
+        if (playerCamera == null){
+            Debug.LogWarning("Ttension y'a pas de camera");
+        }
+        playerCharacter = transform.GetChild(1);
+        agent = playerCharacter.GetComponent<NavMeshAgent>();
+    }
+
+}
